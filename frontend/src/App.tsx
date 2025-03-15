e9954adfe9b59b1a9360ed0ca6e8c00bf40cfb28
MexMass <ramonas.lukas@gmail.com>
2025-03-15 21:38:53 +0000

    add pictures

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import { Navbar } from "./components/layout/Navbar"
 import { Footer } from "./components/layout/Footer"
 import Home from "./pages/Home"
-// import About from "./pages/About"
+import About from "./pages/About"
 // import Products from "./pages/Products"
 // import Contact from "./pages/Contact"
 
@@ -14,8 +14,8 @@ function App() {
         <main className="flex-grow">
           <Routes>
             <Route path="/" element={<Home />} />
-            {/* <Route path="/about" element={<About />} />
-            <Route path="/products" element={<Products />} />
+            <Route path="/about" element={<About />} />
+            {/* <Route path="/products" element={<Products />} />
             <Route path="/contact" element={<Contact />} /> */}
           </Routes>
         </main>
